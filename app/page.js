a3fe721d0dd2ecad4d5a3616c080bf04ca2949ee
Alejandro Bermúdez <mejorandro@gmail.com>
2026-02-25 20:33:32 -0600

    (optimizations): Use image optimizations and also optimize get from cloudinary, something else is that I added metadata, also is dynamic :)

--- app/page.js
+++ app/page.js
@@ -3,6 +3,23 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+//Static metadata
+//export const metadata = {
+//  title: 'Latest Posts',
+//  description: 'Browse our latest posts!'
+//}
+
+//Dynamic metadata
+export async function generateMetadata(){
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `Browse all our ${numberOfPosts} posts!`,
+    description: 'Browse all our posts.'
+  }
+}
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
